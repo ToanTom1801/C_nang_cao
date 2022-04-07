f33d44f34b96470747b5dd76c7044c2b5cc82c5f
ThienToan_89 <100526950+ToanTom1801@users.noreply.github.com>
2022-04-07 15:22:03 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,15 +1,15 @@
+//Phần A
 #include <iostream>
 #include <math.h>
 using namespace std;
  
 int main()
 {
-    // phần A
     int n;
-    cout << "\nNhap n = ";
+    cout << "Nhap so n = ";
     cin >> n;
     if(n < 2){
-        cout << n << "khong phai so nguyen to\n";
+        cout << n << " khong phai so nguyen to\n";
         return 0;
     }
     int count = 0;
@@ -19,8 +19,62 @@ int main()
         }
     }
     if(count == 0){
-        cout << n << "chinh la so nguyen to\n";
+        cout << n << " chinh la so nguyen to\n";
     }else{
-        cout << n <kh phai la so nguyen to\n
+        cout << n << " khong phai so nguyen to\n";
+    }
+}
+
+//Phần B
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    int soTemp;
+    int tong = 0;
+    cout << "Nhap n: ";
+    cin >> n;
+    soTemp = n;
+    while (soTemp != 0) {
+        tong += soTemp % 10;
+        soTemp /= 10;
+    }
+    cout << "Tong cac chu so cua so " << n << " la " << tong << endl;
+    return 0;
+}
+
+//Phần C
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cout << "Nhap so n = ";
+    cin >> n;
+    if(n < 2){
+        cout << n << " khong phai so nguyen to\n";
+        return 0;
+    }
+    int count = 0;
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+    if(count == 0){
+        cout << n << " la so nguyen to\n";
+        int soTemp;
+    	int tong = 0;
+    	soTemp = n;
+    	while (soTemp != 0) {
+        	tong += soTemp % 10;
+        	soTemp /= 10;
+   		}
+    	cout << "Tong cac chu so cua so " << n << " la " << tong << endl;
+    }else{
+        cout << n << " khong phai so nguyen to\n";
     }
 }
