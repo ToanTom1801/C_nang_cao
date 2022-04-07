2b54d14e51980e8b87610762d59856bdad0b0c2c
ThienToan_89 <100526950+ToanTom1801@users.noreply.github.com>
2022-04-07 15:11:23 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+    // phần A
+    int n;
+    cout << "\nNhap n = ";
+    cin >> n;
+    if(n < 2){
+        cout << n << "khong phai so nguyen to\n";
+        return 0;
+    }
+    int count = 0;
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+    if(count == 0){
+        cout << n << "chinh la so nguyen to\n";
+    }else{
+        cout << n << "khong phai la so nguyen to\n";
+    }
+}
